34019ecc82a898d42ed8518a0aafbd4b8191a2ae
akarag05 <karageorgisa1997@gmail.com>
2025-11-24 23:13:12 +0200

    [AK] connected to azure db

---
+++ src/main/java/com/zalex/employee_certification/entities/Certificate.java
@@ -0,0 +1,30 @@
+package com.zalex.employee_certification.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "zalex-certificate")
+public class Certificate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "address_to", nullable = false)
+    private String addressTo;
+    @Column(name = "purpose", nullable = false)
+    private String purpose;
+    @Column(name = "issued_on", nullable = false)
+    private Date issuedOn;
+    @Column(name = "employee_id", nullable = false)
+    private Integer employeeId;
+}
