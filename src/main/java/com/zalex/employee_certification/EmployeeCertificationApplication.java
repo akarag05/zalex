773124db11e59676e3da2e2b7fff5dc92ecd4a40
akarag05 <karageorgisa1997@gmail.com>
2025-11-24 21:15:51 +0200

    [AK] init commit

---
+++ src/main/java/com/zalex/employee_certification/EmployeeCertificationApplication.java
@@ -0,0 +1,13 @@
+package com.zalex.employee_certification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCertificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCertificationApplication.class, args);
+	}
+
+}
